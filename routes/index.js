075985b632d049da00bc65aa36449cdc548c1e29
cgarciasoft <garciacruz87@gmail.com>
2015-07-19 13:06:46 +0200

    Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* Página de entrada (home page) */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
@@ -12,8 +12,9 @@ router.get('/author', function(req, res) {
   res.render('author', {author: ' Carlos Garcia Cruz'});
 });
 
-//controller routes
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Definicion de rutas /quizes/answer
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
