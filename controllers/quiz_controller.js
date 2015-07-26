9c6c5b0456de62a9a68e97f71654a3a5b20ce14d
cgarciasoft <garciacruz87@gmail.com>
2015-07-26 13:05:50 +0200

    Editar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -64,3 +64,26 @@ exports.create = function(req, res) {
 		}
 	})	
 };
+
+//GET /quizes/:quizId/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz;
+	
+	res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+//put /quizes/:quizId/
+exports.update = function(req, res) {
+	req.quiz.pregunta =  req.body.quiz.pregunta;
+	req.quiz.respuesta =  req.body.quiz.respuesta;
+	
+	req.quiz
+	.validate()
+	.then(function(err){
+		if (err){
+			res.render('quizes/edit', {quiz: req.quiz, errors:err.errors});
+		} else{
+			req.quiz.save({fields: ["pregunta","respuesta"]}).then(function(){res.redirect('/quizes');})
+		}
+	})
+};
